83553220233ce9f57c81b39b2eb3d2e547559f45
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-08-14 18:48:55 +0600

    refactor: add left and right padding for space in mobile

--- src/app/(auth)/layout.js
+++ src/app/(auth)/layout.js
@@ -8,7 +8,7 @@ export default function AuthLayout({ children }) {
     return (
         <main className="relative min-h-screen w-full">
             <div className="absolute inset-0 bg-[url('/svg/grain-bg.svg')] bg-cover bg-center bg-repeat"></div>
-            <div className="relative z-10 flex min-h-screen flex-col items-center">
+            <div className="relative z-10 flex min-h-screen flex-col items-center px-5">
                 <div className="flex w-full flex-1 items-center justify-center">{children}</div>
                 <AuthFooter />
             </div>
